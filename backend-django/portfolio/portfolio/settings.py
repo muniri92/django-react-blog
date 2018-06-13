e13248931e4706e8f1f7a2a7a910230b417d1586
Munir Ibrahim <mibrah04@gmail.com>
2018-06-13 00:27:28 -0700

    secrets

--- backend-django/portfolio/portfolio/settings.py
+++ backend-django/portfolio/portfolio/settings.py
@@ -20,7 +20,8 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY')
+SECRET_KEY = ')vk2mr7k_+3!2i=e22c#rhw2f6q753)6w3m5l(_14ciuvujx)p'
+# SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
