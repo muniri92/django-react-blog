969ff303034ec68fecd5a061314cb47721c992a3
Munir Ibrahim <mibrah04@gmail.com>
2018-06-13 00:23:21 -0700

    added static files

--- backend-django/portfolio/portfolio/settings.py
+++ backend-django/portfolio/portfolio/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
@@ -130,3 +130,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = 'staticfiles'
